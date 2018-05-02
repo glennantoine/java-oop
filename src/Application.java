6ef1c0e76a09e2626b8a6bafa198259e4f78c507
Glenn Antoine <me@glennantoine.com>
2018-05-02 11:34:56 -0400

    adding a base class/file

---
+++ src/Application.java
@@ -0,0 +1,17 @@
+/**
+ * Base class for working with code/examples for the
+ * various topics that are to be covered.
+ *
+ * @author  Glenn Antoine
+ * @version 1.0
+ * @since   2018-05-02
+ */
+
+public class Application {
+
+    public static void main(String[] args){
+
+
+    }
+    
+}
